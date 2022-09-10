010bcada311ecba5609b7d6640547f0665cd44f9
zaquel de souza <zaquelsousa2013@hotmail.com>
2022-09-10 19:43:02 -0300

    send notification

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/resquest";
 import "./styles.css";
-function NotificationButton() {
+
+
+type props = {
+    saleId:  number;
+}
+function handleClick(id :number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response =>{
+            console.log("socuesso")
+        })
+}
+
+
+function NotificationButton( {saleId} : props) {
     return (
-        <div className="dsmeta-red-btn-conteiner">
+        <div className="dsmeta-red-btn-conteiner" onClick={()=>handleClick(saleId)}>
             <div className="dsmeta-red-btn">
                 <img src={icon} alt="enviar"/>
             </div>
